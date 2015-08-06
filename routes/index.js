a329ec444a2d681662a66e1b8c3f6e1bb98cfc2a
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-08-06 12:53:30 -0600

    added crud routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
+require('dotenv').load();
 var express = require('express');
 var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI);
+var companies = db.get("companies");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/:id', function(req, res, next) {
+  companies.findOne({userId: req.params.id}, function(err, doc) {
+    if (doc === null) {
+      res.status(404).json({error: "No user id matching"});
+    }
+    res.status(200).json(doc);
+  });
+});
+
+router.post('/create/:id', function(req, res, next) {
+  companies.findOne({userId: req.params.id}, function(err, doc) {
+    if (doc === null) {
+      companies.insert({userId: req.params.id, companies: []}, function(err, doc) {
+        res.end();
+      });
+    } else {
+        res.end();
+    }
+  });
+});
+
+router.post('/delete/:id', function(req, res, next) {
+  companies.remove({userId: req.params.id}, function(err, doc) {
+    res.end();
+  });
+});
+
+router.post('/:id', function(req, res, next) {
+  companies.findOne({userId: req.params.id}, function(err, doc) {
+    var object = doc;
+    object.companies.push(req.body);
+    companies.findAndModify({userId: req.params.id}, object, function (err, doc) {
+      res.end();
+    });
+  });
 });
 
 module.exports = router;
